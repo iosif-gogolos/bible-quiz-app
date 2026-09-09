a220c8375726bf97eaebbf910c4637ef0650f6e4
iosif-gogolos <iosif.gogolos@icloud.com>
2026-09-09 16:54:42 +0200

    Update config

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -5,11 +5,8 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     console.error(
-        'Missing Supabase configuration! Ensure CONFIG_SUPABASE_URL and CONFIG_SUPABASE_ANON_KEY are set in your .env file.'
+        'Missing Supabase configuration! Ensure VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY are set.'
     );
 }
 
-export const supabase = createClient(
-    supabaseUrl || '',
-    supabaseAnonKey || ''
-);
+export const supabase = createClient(supabaseUrl || '', supabaseAnonKey || '');
